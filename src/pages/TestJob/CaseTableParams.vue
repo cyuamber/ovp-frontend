099075175ad7f7e2eacf726615cd0a87241e9b33
houlijie <34570372@qq.com>
2021-01-24 16:15:52 +0800

    feat: 增加是否必填项

--- src/pages/TestJob/CaseTableParams.vue
+++ src/pages/TestJob/CaseTableParams.vue
@@ -9,7 +9,8 @@
     <template>
       <a-table
         :columns="columnsTitle"
-        :data-source="tabData ? tabData : []"
+        :data-source="tabData"
+        :scroll="{ x: 1300 }"
         bordered
       >
         <template
@@ -54,29 +55,102 @@
 
 <script type="text/ecmascript-6">
 import { mapState,mapMutations } from "vuex"
-let valArr;
+let valArr,jsonParameters,optionalParam=[];
 export default {
   name: 'CaseTableParams',
   props: ['isEdit'],
   data() {
-    // this.initRanderFn()
+     this.init = () => {
+          let caseParamsIsShow = this.$store.state.testJob.caseParamsIsShow
+          let valObj = {}
+          let tabDataArr=[];
+         let dataTab =  JSON.parse(sessionStorage.getItem('tabdata'))
+         console.log('dataTab',dataTab)
+            let json;
+          if(caseParamsIsShow && dataTab.parameters.length>0 ){
+             if(this.caseParams && this.caseParamsData.parameters.length<=0){
+                 json = this.caseParamsData.parameters
+              }else{
+                 json = dataTab.parameters
+              }
+              let nameItem=[]
+
+            json.map((item)=>{
+               const nameText = item.isOptional?'*'+item.name:item.name
+              if(item.defaultValue===null){
+                item.defaultValue = ''
+              }
+              valObj[nameText] = item.defaultValue.split(';')
+              if(item.visible){//如果是true的话展示列
+
+                //console.log('nameText',nameText)
+                nameItem.push(nameText)
+              }
+              if(item.isOptional){
+                optionalParam.push(nameText)
+              }
+            })
+          let obj = {};
+          console.log('nameItem',nameItem)
+            nameItem.map((val)=>{
+            let  nameLen = nameItem.length,valLen = valObj[val].length;
+            console.log(valLen,nameLen)
+            if(!val.defaultValue){
+              let fillArr = Array.from({length:nameLen},()=> '--');
+                valObj[val] = [...valObj[val], ...fillArr]
+            }else if(valLen<nameLen){
+                  let fillLen = nameLen-valLen;
+                  let fillArr = Array.from({length:fillLen},()=> '--');
+                  valObj[val] = [...valObj[val], ...fillArr]
+              }
+              if(!obj.val){
+                obj[val]='--'
+                tabDataArr.push(obj)
+              }
+            })
+            var copytab = JSON.parse(JSON.stringify(tabDataArr))||[];
+            for(var n in valObj){
+                copytab.map((ele,i)=>{
+                    switch(n){
+                      case n:
+                        ele[n] =  valObj[n][i];
+                        ele["key"] = i;
+                        break;
+                  }
+                })
+            }
+          }
+          this.tabData = copytab
+          if(copytab && copytab.length>0){
+             this.cacheData = copytab.map(item => ({ ...item }))
+          }
+
+           return copytab
+    }
+   let data = this.init()||[];
     return {
-      tabData:[],
+      tabData: data,
       editingKey : '',
       count: 0,
       cacheData:null,
       caseData:[],
+      flag:false,
       title: this.isEdit ? 'Edit Case Parameters' : 'Add Case Parameters',
     }
   },
   created(){
 
   },
-  mounted(){
-    //setTimeout(()=>{
-      // sessionStorage.setItem('tabdata', JSON.stringify(this.$store.state.testJob.caseParamsData))
-      this.initRanderFn()
-   // },1000)
+  updated(){
+    let storage = window.sessionStorage;
+    let lastname = JSON.parse(storage.getItem("tabdata"));
+
+    if( lastname && lastname.parameters && lastname.parameters.length > 0){
+      if(!this.flag){
+          this.init()
+          this.flag = true
+      }
+    }
   },
   computed: {
     ...mapState({
@@ -96,12 +170,13 @@ export default {
       }
       if(this.caseParamsData.parameters){
         this.caseParamsData.parameters.map((item,key)=>{
+          const nameText = item.isOptional?'*'+item.name:item.name
           columnsArr.push({
-            title:item.name,
-            dataIndex: item.name,
+            title:nameText,
+            dataIndex: nameText,
             key:key,
             editable:false,
-            scopedSlots: { customRender: item.name }
+            scopedSlots: { customRender: nameText }
           })
         })
         columnsArr.push(onlyOperation)
@@ -113,7 +188,8 @@ export default {
       const columnsHead = []
        if(this.caseParamsData.parameters){
         this.caseParamsData.parameters.map((item)=>{
-            columnsHead.push(item.name)
+          const nameText = item.isOptional?'*'+item.name:item.name
+            columnsHead.push(nameText)
         })
        }
         return columnsHead
@@ -125,7 +201,7 @@ export default {
         this.count++
         if (this.count > 1 && this.caseParamsData&&this.caseParamsData.parameters.length>0) { // 只有首次用的initialValue，后面每次打开都重新设置值
           this.caseParams = this.caseParamsData.parameters.filter((item) => {
-           //  this.columnsData.push(item.name)
+            // this.columnsData.push(item.name)
             return item.visible !== false;
           });
 
@@ -154,71 +230,13 @@ export default {
 
 
     },
-    initRanderFn(){
-       // let caseParamsData = this.$store.state.testJob.caseParamsData;
-    let caseParamsIsShow = this.$store.state.testJob.caseParamsIsShow
-     let valObj = {}
-     let dataTab =  JSON.parse(sessionStorage.getItem('tabdata'))
-     let json;
-    if(caseParamsIsShow && dataTab.parameters.length>0 ){
-      if(this.caseParamsData.parameters.length<=0){
-        json = dataTab.parameters
-      }else{
-        json = this.caseParamsData.parameters
-      }
-
-        let nameItem=[]
-
-      json.map((item)=>{
-        if(item.defaultValue===null){
-          item.defaultValue = ''
-        }
-        valObj[item.name] = item.defaultValue.split(';')
-        // if(item.visible){//如果是true的话展示列
-         nameItem.push(item.name)
-        // }
-
-      })
-     let obj = {};
-      nameItem.map((val)=>{
-       let  nameLen = nameItem.length,valLen = valObj[val].length;
-       if(!valLen){
-         let fillArr = Array.from({length:nameLen},()=> '--');
-          valObj[val] = [...valObj[val], ...fillArr]
-       }else if(valLen<nameLen){
-            let fillLen = nameLen-valLen;
-            let fillArr = Array.from({length:fillLen},()=> '--');
-            valObj[val] = [...valObj[val], ...fillArr]
-        }
-        if(!obj.val){
-           obj[val]='--'
-           this.tabData.push(obj)
-        }
-       })
-      var copytab = JSON.parse(JSON.stringify(this.tabData))
-     for(var n in valObj){
-        copytab.map((ele,i)=>{
-             switch(n){
-               case n:
-                ele[n] =  valObj[n][i];
-                ele["key"] = i;
-                break;
-           }
-         })
-     }
-    this.tabData = copytab
-    this.cacheData = copytab.map(item => ({ ...item }))
-    return copytab
-
-    }
-    },
     // 取消弹层的逻辑
      handleCancel() {
       // 将值恢复
       this.caseParams = this.caseParamsData.parameters.filter((item) => {
         return item.visible !== false
       })
-      this.initRanderFn();
+      this.init();
       this.setCaseParamsIsShow(false)
     },
     strBool(val) {
@@ -227,19 +245,23 @@ export default {
     handleChange(value, key, column) {
       const newData = [...this.tabData]
       const target = newData.filter(item => key === item.key)[0]
-
-      this.caseData = JSON.parse(JSON.stringify(this.$store.state.testJob.caseParamsData.parameters));
+      if(!jsonParameters){
+      let  jsonArr = [{"name":"config-json","value":"/opt/oclip/conf/tmp/e8e5ac66-5716-11eb-b64b-fa163e554a15.json","description":"Configuration file","type":"string","defaultValue":null,"isOptional":true,"visible":false},{"name":"instrument-ids","value":null,"description":"Auxiliary parameters, instrument vm ids of instrument","type":"string","defaultValue":null,"isOptional":true,"visible":false},{"name":"instrument-ips","value":null,"description":"Instrument vm ips of seagull","type":"string","defaultValue":null,"isOptional":true,"visible":true},{"name":"caps","value":null,"description":"Call rate of seagull case","type":"string","defaultValue":null,"isOptional":true,"visible":true},{"name":"number-calls","value":null,"description":"Number-calls of seagull case","type":"string","defaultValue":null,"isOptional":false,"visible":true},{"name":"sutaddress","value":null,"description":"Sut address for DRA","type":"string","defaultValue":null,"isOptional":true,"visible":true},{"name":"protocol","value":null,"description":"Protocol of seagull case","type":"string","defaultValue":null,"isOptional":true,"visible":true},{"name":"mode","value":null,"description":"Mode of seagull case","type":"string","defaultValue":null,"isOptional":true,"visible":false},{"name":"timeout","value":"60000","description":"timeout for command to complete the given task in milliseconds","type":"string","defaultValue":"60000","isOptional":true,"visible":true}]
+        // jsonParameters = JSON.parse(JSON.stringify(this.$store.state.testJob.caseParamsData.parameters));
+        jsonParameters = JSON.parse(JSON.stringify(jsonArr));
+      }
+    //  this.caseData = JSON.parse(JSON.stringify(this.$store.state.testJob.caseParamsData.parameters));
       if (target) {
         target[column] = value
 
         this.tabData = newData
-        this.caseData.map((item)=>{
+       jsonParameters.map((item)=>{
             if(item.name==column){
               if(item.defaultValue===null){
                 item.defaultValue = ''
               }
               if(item.value===null){
-                item.value=''
+                item.value = ''
               }
                valArr =  item.defaultValue.split(';')
                valArr.splice(key,1,value)
@@ -249,6 +271,7 @@ export default {
 
             return item;
         })
+       this.caseData = jsonParameters
       }
     },
     edit(key) {
@@ -265,6 +288,16 @@ export default {
       const newCacheData = [...this.cacheData]
       const target = newData.filter(item => key === item.key)[0]
       const targetCache = newCacheData.filter(item => key === item.key)[0]
+      console.log('optionalParam',optionalParam,target)
+      // const keysOption = Object.keys(optionalParam)
+      // for(let n in target){
+        for(var i=0;i<optionalParam.length;i++){
+         if(!target[optionalParam[i]]){
+           this.$message.error(optionalParam[i]+' is required')
+           return;
+         }
+        }
+      // }
       if (target && targetCache) {
         delete target.editable
         this.tabData = newData
@@ -290,11 +323,5 @@ export default {
 <style lang="less" scope>
 .editable-row-operations a {
   margin-right: 8px;
-}
-</style>
-<style>
-.ant-table-wrapper {
-  width: 100%;
-  overflow: auto;
 }
 </style>
